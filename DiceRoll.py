496ac2118305effdc7169d68160bf446e0a23d4b
jkosmicki2024 <jkosmicki@unomaha.edu>
2025-02-26 15:10:58 +0000

    Working lab

--- DiceRoll.py
+++ DiceRoll.py
@@ -1,6 +1,6 @@
 #DiceRoll.py
-#Name:
-#Date:
+#Name: Jacob Kosmicki
+#Date: 2/26/2025
 #Assignment:
 import random
 
@@ -9,6 +9,11 @@ def main():
   rolls = [0,0,0,0,0,0,0,0,0,0,0,0]
   #Create two dice values ranging from 1 - 6 each
   
+  dice1 = random.randint(1,6)
+  dice2 = random.randint(1,6)
+  total = dice1 + dice2
+
+
   #find the sum total of the two dice
   
   #print statictics for dice rolls
