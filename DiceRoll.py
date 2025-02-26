e7f99e953f53ce66f0189ff092a6a0b4ab740a77
jkosmicki2024 <jkosmicki@unomaha.edu>
2025-02-26 15:19:06 +0000

    lab

--- DiceRoll.py
+++ DiceRoll.py
@@ -6,13 +6,18 @@ import random
 
 def main():
   #Create an empty list with possible roll values
-  rolls = [0,0,0,0,0,0,0,0,0,0,0,0]
+  rolls = [0,0,0,0,0,0,0,0,0,0,0]
   #Create two dice values ranging from 1 - 6 each
   
   dice1 = random.randint(1,6)
   dice2 = random.randint(1,6)
   total = dice1 + dice2
 
+  rolls[total -2] = rolls[total -2] + 1
+
+  print(dice1, dice2, total)
+  print(rolls)
+  
 
   #find the sum total of the two dice
   
